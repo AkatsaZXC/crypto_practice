239612ba5a0b133ce8613d71f21f215be64de1f6
AkatsaZXC <akatsaaaaa@gmail.com>
2021-04-19 20:14:55 -0700

    added else statement

--- crypto_pr1/practical_work1/polibian_square.py
+++ crypto_pr1/practical_work1/polibian_square.py
@@ -44,7 +44,6 @@ def encryption():
         for i, k in enumerate(alphabet_list):
             alphabet_dict[k] = values[i]
         print()
-        print(alphabet_dict)
 
         fraze = input("Введите фразу для шифрования:\n")
         fraze = fraze.upper()
@@ -64,7 +63,42 @@ def encryption():
         print(output)
 
     elif result == 'N':
-        pass
+        alphabet_list = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                         'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', '.', '!', '-']
+
+        print("Ваш алфавит:")
+        for s in alphabet_list:
+            print(f"{s}", end='')
+        print()
+
+        #  Создание словаря со значениями для шифрования
+        alphabet_dict = {}
+        values = [11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25, 26, 31, 32, 33, 34, 35, 36, 41, 42, 43, 44, 45, 46, 51,
+                  52,
+                  53, 54, 55, 56, 61, 62, 63, 64, 65, 66]
+        values = list(map(str, values))
+        #  Заполнение словаря значениями из values и ключами из alphabet_list
+        i = 0
+        for i, k in enumerate(alphabet_list):
+            alphabet_dict[k] = values[i]
+        print()
+
+        fraze = input("Введите фразу для шифрования:\n")
+        fraze = fraze.upper()
+
+        #  Кодирование сообщения по значению по ключу из словаря
+        def decoder(fraze):
+            new_fraze = ""
+            #  list_fraze = list(def_fraze)
+            for x in fraze:
+                if x in alphabet_dict:
+                    new_fraze += alphabet_dict[x]
+                else:
+                    new_fraze += x
+            return new_fraze
+
+        output = decoder(fraze)
+        print(output)
     else:
         print('Выход')
         exit()
