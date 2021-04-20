e91a430e45306e41deed1e7c5f4d2aefa00a6232
AkatsaZXC <akatsaaaaa@gmail.com>
2021-04-20 06:49:11 -0700

    some stuff with input/output

--- crypto_pr1/practical_work1/practical_work1_main.py
+++ crypto_pr1/practical_work1/practical_work1_main.py
@@ -8,6 +8,7 @@ ________________________________
     
     [1] Лозунговый шифр
     [2] Полибианский квадрат
+    [3] Шифр Тритемия
     """)
 
     cipher_number = input("Выберите номер шифра:\n")
@@ -21,6 +22,12 @@ ________________________________
     if cipher_number == '2':
         try:
             polibian_square.main()
+        except KeyboardInterrupt:
+            print("Выход...")
+            exit()
+    if cipher_number == '3':
+        try:
+            trithemius_cipher.main()
         except KeyboardInterrupt:
             print("Выход...")
             exit()
--- crypto_pr1/practical_work1/trithemius_cipher.py
+++ crypto_pr1/practical_work1/trithemius_cipher.py
@@ -1,5 +1,32 @@
 def encryption():
-    pass
+    print("Исходная таблица: ")
+    alphabet_list = [['А', 'Б', 'В', 'Г', 'Д', 'Е'],
+                     ['Ё', 'Ж', 'З', 'И', 'Й', 'К'],
+                     ['Л', 'М', 'Н', 'О', 'П', 'Р'],
+                     ['С', 'Т', 'У', 'Ф', 'Х', 'Ц'],
+                     ['Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь'],
+                     ['Э', 'Ю', 'Я', '-', '-', '-']]
+    print()
+    #  Вывод алфавита в коносоль
+    for i in range(len(alphabet_list)):
+        for j in range(len(alphabet_list[i])):
+            print(alphabet_list[i][j], end=' ')
+        print()
+    fraze = input("Напишите ключевое слово:\n").upper()
+    fraze_list = list(fraze)
+    print("Ваше ключевое слово: ", end=' ')
+    print(fraze)
+
+    #  Удаление повторяющихся символов в ключевом слове
+    sorted_fraze = list()
+    for s in fraze_list:
+        if s not in sorted_fraze:
+            sorted_fraze.append(s)
+    '''Дополнение элементами сортированного ключ. слова исходного алфавита,
+    удаля повторяющиеся буквы'''
+
+    str_to_encrypt = input("Введите строку для шифрования: ").upper()
+    print("Ваша строка", end=' ')
 
 
 def main():
