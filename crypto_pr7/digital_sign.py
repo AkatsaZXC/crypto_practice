009528fe91439e0e4a97776d9ba8aa5485a685d4
Leonid <akatsaaaaa@gmail.com>
2021-05-16 11:41:10 -0700

    added digital_sign verification

--- crypto_pr7/digital_sign.py
+++ crypto_pr7/digital_sign.py
@@ -1,6 +1,17 @@
 import rsa
 
+
 def encryption():
+    # Генерация пары ключей
+    (pubkey, privkey) = rsa.newkeys(2048)
+
+    message_for_encryption = bytes(input("Введите сообщение для шифрования: "), encoding='utf-8')
+
+    # Подписка сообщения цифровой подписью
+    signature = rsa.sign(message_for_encryption, privkey, 'SHA-256')
+
+    # Проверка цифровой подписи
+    print(rsa.verify(message_for_encryption, signature, pubkey))
 
 
 def main():
