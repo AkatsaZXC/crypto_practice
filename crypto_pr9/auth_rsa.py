f086ec63db68b294a72b6b98f76df1f5b6c20f94
Leonid <akatsaaaaa@gmail.com>
2021-05-16 15:57:26 -0700

    added random key choice

--- crypto_pr9/auth_rsa.py
+++ crypto_pr9/auth_rsa.py
@@ -1,9 +1,19 @@
-def encryption():
-    pass
+import rsa
+import random
+
+# Генерация рандомной длины ключа
+random_key_size_list = [128, 256, 512, 1024, 2048, 4096]
+random_key_size = random.choice(random_key_size_list)
+# Генерация пары ключей
+(pubkey, privkey) = rsa.newkeys(random_key_size)
+
+def auth():
+    info_to_check = pubkey
+    print(pubkey)
 
 
 def main():
-    encryption()
+    auth()
 
 
 if __name__ == '__main__':
