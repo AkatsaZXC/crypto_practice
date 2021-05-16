289112ab0bbc59eef707e19f342418a230fc2c41
Leonid <akatsaaaaa@gmail.com>
2021-05-16 11:58:25 -0700

    done rsa digital_sign_verification

--- crypto_pr7/digital_sign.py
+++ crypto_pr7/digital_sign.py
@@ -10,8 +10,24 @@ def encryption():
     # Подписка сообщения цифровой подписью
     signature = rsa.sign(message_for_encryption, privkey, 'SHA-256')
 
+    check = input("Желаете изменить сообщение?(Y/N): ").upper()
+    if check == 'Y':
+        print("Введите сообщение: ", end="")
+        new_message = bytes(input(), encoding='utf-8')
+        try:
+            result = rsa.verify(new_message, signature, pubkey)
+            print(result)
+        except rsa.VerificationError:
+            print("Ошибка верификации...")
+    elif check == 'N':
+        print("Проверка подписи: ")
+        result = rsa.verify(message_for_encryption, signature, pubkey)
+        print(result)
+        print("Верификация успешна!")
+    else:
+        print("Выход...")
     # Проверка цифровой подписи
-    print(rsa.verify(message_for_encryption, signature, pubkey))
+    # print(rsa.verify(message_for_encryption, signature, pubkey))
 
 
 def main():
